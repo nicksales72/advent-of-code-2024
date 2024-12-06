94001753ff797d9e53f2239a2335f68220ddfc70
nicksales7 <nicksales72@gmail.com>
2024-12-06 09:47:00 -0500

    day3 pt2

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -6,7 +6,21 @@ def file_to_text(f) -> str:
     return text
 
 def find_muls(t) -> list[str]:
-    return re.findall(r"mul\(\d+,\d+\)", t)
+    return re.findall(r"(?:mul\(\d+,\d+\)|do\(\)|don't\(\))", t)
+
+def remove_muls(lst) -> list[str]:
+    new_lst = []
+    flag = True
+    for i in lst:
+        if i == "don't()":
+            flag = False
+            continue
+        if i == "do()":
+            flag = True
+            continue
+        if flag:
+            new_lst.append(i)
+    return new_lst
 
 def multiply_pairs(t) -> int:
     sum = 0 
@@ -18,4 +32,6 @@ def multiply_pairs(t) -> int:
     return sum
 
 txt = file_to_text("day3.txt")
-print(multiply_pairs(find_muls(txt)))
+muls = find_muls(txt)
+muls_pt2 = remove_muls(muls)
+print(multiply_pairs(muls), multiply_pairs(muls_pt2))
